28627dea7b7acd18ec008058a337b71dc234144c
Jairam2201 <140802567+Jairam2201@users.noreply.github.com>
2024-03-31 13:58:23 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,21 @@
+class login:
+    def login(self):
+        k=0
+        while k<1:
+            self.user=input("Enter username:")
+            self.pswd=input("Enter password:")
+            import csv
+            with open("reg1.csv","r",newline="") as file:
+                store=csv.DictReader(file)
+                for i in store:
+                    if self.user==i["user"] and self.pswd==i["pswd"]:
+                        print("Login SUCCESS")
+                        k=1
+                        break
+                if self.user!=i["user"] or self.pswd!=i["pswd"]:
+                    print("Incorrect login credentials")
+                        
+            
+        
+obj=login() 
+obj.login()
