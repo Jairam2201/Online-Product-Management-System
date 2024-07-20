778e4d6caabc5d7ff7b29fbb3a253d003fef6339
Jairam2201 <140802567+Jairam2201@users.noreply.github.com>
2024-07-20 18:35:28 +0530

    Update registration.py

--- registration.py
+++ registration.py
@@ -1,10 +1,21 @@
 import csv
 def reg():
-    f=open("reg1.csv","a",newline="")
+    f=open("registration.csv","a",newline="")
     a=csv.writer(f)
     print("!!!WELCOME TO AMAZON!!!")
     studentid=input("Enter an ID:")
-    user=input("Enter a useranme:")
+    i=True
+    while i:
+        user=input("Enter a useranme:")
+        with open("registration.csv","r",newline="") as file:
+                    store=csv.DictReader(file)
+                    for i in store:
+                        if user==i["User"]:
+                            print("Username already exists.")
+                            i=True
+                            break
+                        else:
+                            i=False
     pswd=input("Enter a password:")
     phno=input("PHNO:")
     a.writerow([studentid,user,pswd,phno])
