7324a4d63d7853cf91f1a43c2895f7fc3c71d374
Jairam2201 <140802567+Jairam2201@users.noreply.github.com>
2024-07-20 18:36:17 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,18 +1,18 @@
+import csv
 class login:
     def login(self):
         k=0
         while k<1:
             self.user=input("Enter username:")
             self.pswd=input("Enter password:")
-            import csv
-            with open("reg1.csv","r",newline="") as file:
+            with open("registration.csv","r",newline="") as file:
                 store=csv.DictReader(file)
                 for i in store:
-                    if self.user==i["user"] and self.pswd==i["pswd"]:
+                    if self.user==i["User"] and self.pswd==i["Pswd"]:
                         print("--->LOGIN SUCCESS<---")
                         k=1
                         break
-                if self.user!=i["user"] or self.pswd!=i["pswd"]:
+                if self.user !=i["User"] or self.pswd !=i["Pswd"]:
                     print("Incorrect login credentials.Please enter again.")
                         
             
